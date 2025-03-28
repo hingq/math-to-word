a2da9ac4deb543746e5b1010e19ba421b7d29266
he <amazyko@foxmail.com>
2025-03-28 20:18:05 +0800

    first

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import {renderToString} from "katex";
+import {onMounted} from "vue";
+
+
+const count: string = "$$E=mc^2$$"
+onMounted(() => {
+  const content = document.getElementById("content")
+  if (content) {
+    content.innerHTML = renderToString(count.replace(/\$\$/g, ""), {
+      throwOnError: true,
+      displayMode: true,
+      output: "mathml"
+    });
+  }
+})
+
+</script>
+
+<template>
+  <div class="card">
+    <input autocomplete="off" autofocus placeholder="Enter your math expression here" type="text"/>
+    <button type="button"></button>
+    <div id="content">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+input {
+  outline: none;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  
+}
+</style>
