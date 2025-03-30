871dd3216ce7e943c60903f37a7cacc13fc89c1d
virgomr <10153675+virgomr@user.noreply.gitee.com>
2025-03-30 22:24:46 +0800

    add

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,89 @@
 <script lang="ts" setup>
 import {renderToString} from "katex";
-import {onMounted} from "vue";
+import {onMounted, ref, watch} from "vue";
 
+const count = ref<string>("");
+const show = ref<string>("");
+const inputfunction = () => {
+  show.value = renderToString(count.value, {
+    throwOnError: false, //避免错误崩溃
+    displayMode: true,
+    output: "mathml",
+  });
+};
+watch(
+    () => {
+      return count.value;
+    },
+    () => {
+      inputfunction();
+    },
+);
 
-const count: string = "$$E=mc^2$$"
 onMounted(() => {
-  const content = document.getElementById("content")
-  if (content) {
-    content.innerHTML = renderToString(count.replace(/\$\$/g, ""), {
-      throwOnError: true,
-      displayMode: true,
-      output: "mathml"
-    });
-  }
-})
-
+});
 </script>
-
 <template>
   <div class="card">
-    <input autocomplete="off" autofocus placeholder="Enter your math expression here" type="text"/>
-    <button type="button"></button>
-    <div id="content">
+    <div class="card-container">
+      <input v-model="count" class="card-body" placeholder="请输入"/>
+      <div id="content" v-html="show"></div>
     </div>
   </div>
 </template>
 
 <style scoped>
-input {
-  outline: none;
+/* 页面整体居中 */
+.card {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+}
+
+/* 卡片容器 */
+.card-container {
+  position: relative;
+  width: 350px;
+  padding: 20px;
+  border-radius: 12px;
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+}
+
+/* 文字输入框 */
+.card-body {
+  background: #1a1a1a;
+  width: 100%;
+  min-height: 40px;
+  padding-left: 10px;
   border: 1px solid #ccc;
-  border-radius: 8px;
-  
+  border-radius: 6px;
+  font-size: 14px;
+  outline: none;
+}
+
+/* LaTeX 公式显示区域 */
+#content {
+  color: #535bf2;
+  border: 1px solid #a0b3d6;
+  width: 100%;
+  min-height: 50px;
+  margin-top: 10px;
+  padding: 5px;
+  border-radius: 6px;
+  background: #f1f1f1;
+  text-align: center;
+}
+
+/* 让彩色边框部分沿四周移动 */
+@keyframes borderMove {
+  0% {
+    background-position: 0% 50%;
+  }
+  100% {
+    background-position: 100% 50%;
+  }
 }
 </style>
